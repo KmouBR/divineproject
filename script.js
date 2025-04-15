5ad6ba74f02cb9674c1471729d6ff4a40fdc18b3
KmouBR <rafaeltvinci@gmail.com>
2025-04-15 00:14:15 -0300

    Start Mobile Chances

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const menuCloseButton = document.querySelector("#menu-close-button");
 const menuOpenButton = document.querySelector("#menu-open-button");
 
 menuOpenButton.addEventListener("click", () => {
-
     document.body.classList.toggle("show-mobile-menu");
 });
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click()); 
